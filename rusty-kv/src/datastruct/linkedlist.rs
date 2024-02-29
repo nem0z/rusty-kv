c9e03054b50933c425c823690cc7de139dce5d91
nem0z <maxime.bernard.dev@gmail.com>
2024-02-29 14:21:59 +0100

    feat (src/datastruct): implement node constructor

--- rusty-kv/src/datastruct/linkedlist.rs
+++ rusty-kv/src/datastruct/linkedlist.rs
@@ -2,6 +2,17 @@ struct Node<T> {
     value: T,
     next: Option<Box<Node<T>>>
 }
+#[allow(dead_code)]
+impl<T> Node<T> {
+    pub fn new(val: T) -> Self {
+        return Node { value: val, next: None };
+    }
+
+    pub fn new_with_next(val: T, next: Option<Box<Node<T>>>) -> Self {
+        return Node { value: val, next: next };
+    }
+}
+
 pub struct Linkedlist<T> {
     head: Option<Box<Node<T>>>
 }
